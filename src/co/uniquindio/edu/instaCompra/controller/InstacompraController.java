f55735fdb81dd335cfc808157c9f8bf34fea4f07
Jhones <jhonosoa@gmail.com>
2021-12-08 15:58:08 -0500

    Se re ajusta el crud de cliente

--- src/co/uniquindio/edu/instaCompra/controller/InstacompraController.java
+++ src/co/uniquindio/edu/instaCompra/controller/InstacompraController.java
@@ -1,12 +1,29 @@
 package co.uniquindio.edu.instaCompra.controller;
 
 import co.uniquindio.edu.instaCompra.Aplicacion;
+import javafx.fxml.FXML;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 
 public class InstacompraController {
 
 	public InstacompraController() {
 		
 	}
+	
+	
+	@FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
 	
 	public void setAplicacion (Aplicacion aplicacion) {
 		
